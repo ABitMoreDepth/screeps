1ebaf394eebe2b043af9769f3aef8f550af2352b
Sam Martin <Nivekkas@gmail.com>
2019-12-31 01:41:59 +0000

    Add costs matrix for creep parts

---
+++ src/utils/creepPartCosts.ts
@@ -0,0 +1,11 @@
+// Body part 	Build cost 	Effect per one body part
+export const CreepPartMatrix: { [index: string]: number } = {
+    ATTACK: 80,
+    CARRY: 50,
+    CLAIM: 600,
+    HEAL: 250,
+    MOVE: 50,
+    RANGED_ATTACK: 150,
+    TOUGH: 10,
+    WORK: 100,
+};
